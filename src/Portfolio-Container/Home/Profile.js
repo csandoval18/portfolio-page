8212d8b2f2927321e4f95b0d89ed251345465249
Christian AS <alvitesscf18@uww.edu>
2022-05-19 21:13:53 -0500

    header progress

--- src/Portfolio-Container/Home/Profile.js
+++ src/Portfolio-Container/Home/Profile.js
@@ -8,30 +8,21 @@ export default function Profile(props) {
 			<div className='profile-parent'>
 				<div className='profile-details'>
 					<div className='colz'>
-						<a href='#'>
-							<i className='fa fa-facebook-square'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-google-plus-square'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-instagram'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-youtube-square'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-twitter'></i>
-						</a>
+						<div className='colz-icon'>
+							<a href='#'>
+								<i className='fa fa-linkedin-square'></i>
+							</a>
+							<a href='#'>
+								<i className='fa fa-github-square'></i>
+							</a>
+						</div>
 					</div>
-
 					<div className='profile-details-name'>
 						<span className='primary-text'>
 							{' '}
 							Hello, I'm <span className='highlighted-text'>Christian</span>
 						</span>
 					</div>
-
 					<div className='profile-details-role'>
 						<span className='primary-text'>
 							{' '}
@@ -43,20 +34,37 @@ export default function Profile(props) {
 									repeat={Infinity}
 									sequence={[
 										'Junior Web Developer',
-										1000,
-										'Junior Full Stack Developer',
-										1000,
+										1500,
 										'LAMP Stack',
-										1000,
+										1500,
 										'MERN Stack',
-										1000,
-										'Enthusiastic Dev',
-										1000,
+										1500,
+										'Mobile Development',
+										1500,
+										'Flutter + Firebase',
+										1500,
 									]}
 								/>
 							</h1>
+							<span className='profile-role-tagline'>
+								University of Wisconsin - Whitewater graduate majoring in
+								Computer Science<br></br>with experience building applications
+								with front end and back end operations.
+							</span>
 						</span>
 					</div>
+					<div className='profile-optins'>
+						<button className='btn primary-btn'>
+							{''}
+							Hire Me{' '}
+						</button>
+						<a href='Resume.pdf' download='CAS_Resume.pdf'>
+							<button className='btn highlighted-btn'>Get Resume</button>
+						</a>
+					</div>
+				</div>
+				<div className='profile-picture'>
+					<div className='profile-picture-background'></div>
 				</div>
 			</div>
 		</div>
